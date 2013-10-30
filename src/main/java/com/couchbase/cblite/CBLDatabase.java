8c7d808baae496dc02aa20221e32196e225b79d3
Traun Leyden <traun@couchbase.com>
2013-10-29 17:58:10 -0700

    Rename pull() -> getPullReplication() to match public api.  Also implement some missing functionality to make it work.

--- src/main/java/com/couchbase/cblite/CBLDatabase.java
+++ src/main/java/com/couchbase/cblite/CBLDatabase.java
@@ -2761,7 +2761,7 @@ public class CBLDatabase {
      */
     @InterfaceAudience.Public
     public CBLReplicator getPushReplication(URL remote) {
-        return getActiveReplicator(remote, true);
+        return manager.replicationWithDatabase(this, remote, true, true, false);
     }
 
     /**
@@ -2773,7 +2773,7 @@ public class CBLDatabase {
      */
     @InterfaceAudience.Public
     public CBLReplicator getPullReplication(URL remote) {
-        return getActiveReplicator(remote, false);
+        return manager.replicationWithDatabase(this, remote, false, true, false);
     }
 
     /**
@@ -2781,10 +2781,10 @@ public class CBLDatabase {
      * returns existing replications if there are any.
      *
      * @param remote
-     * @param exclusively - this param is ignored!  TODO: fix this
      * @return An array whose first element is the "pull" replication and second is the "push".
      */
-    public List<CBLReplicator> replicate(URL remote, boolean exclusively) {
+    @InterfaceAudience.Public
+    public List<CBLReplicator> getReplications(URL remote) {
         CBLReplicator pull;
         CBLReplicator push;
         if (remote != null) {
@@ -2826,6 +2826,7 @@ public class CBLDatabase {
         return null;
     }
 
+    @InterfaceAudience.Private
     public CBLReplicator getReplicator(URL remote, HttpClientFactory httpClientFactory, boolean push, boolean continuous, ScheduledExecutorService workExecutor) {
         CBLReplicator result = getActiveReplicator(remote, push);
         if(result != null) {
