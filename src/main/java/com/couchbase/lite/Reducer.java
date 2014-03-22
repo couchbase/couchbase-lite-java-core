cc4c79e4197e08ee128ee877b03a20af99f1033f
Marius Volkhart <Marius@volkhart.com>
2014-03-22 01:36:03 -0500

    Rename variable to avoid AS complaining about a template variable
    Makes the suggested variable in Android Studio not get marked as
    being spelled incorrectly.

--- src/main/java/com/couchbase/lite/Reducer.java
+++ src/main/java/com/couchbase/lite/Reducer.java
@@ -12,9 +12,9 @@ public interface Reducer {
      *
      * @param keys An array of keys to be reduced (or null if this is a rereduce).
      * @param values A parallel array of values to be reduced, corresponding 1::1 with the keys.
-     * @param rereduce true if the input values are the results of previous reductions.
+     * @param reReduce true if the input values are the results of previous reductions.
      * @return The reduced value; almost always a scalar or small fixed-size object.
      */
-    Object reduce(List<Object> keys, List<Object> values, boolean rereduce);
+    Object reduce(List<Object> keys, List<Object> values, boolean reReduce);
 
 }
