56c422457bd10f45f2ade5c3777197806ba0b241
Traun Leyden <traun@couchbase.com>
2014-05-01 16:15:23 -0700

    Remove log noise

--- src/main/java/com/couchbase/lite/replicator/BulkDownloader.java
+++ src/main/java/com/couchbase/lite/replicator/BulkDownloader.java
@@ -211,7 +211,6 @@ public class BulkDownloader extends RemoteRequest implements MultipartReaderDele
         if (_docReader == null) {
             throw new IllegalStateException("_docReader is not defined");
         }
-        Log.v(Log.TAG_SYNC, "%s: appending data to document", this);
         _docReader.appendData(data);
     }
 
