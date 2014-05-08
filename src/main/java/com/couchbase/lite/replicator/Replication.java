d34d86045df37e4fd0fbc54e914a9d5c1d4233f0
Traun Leyden <traun@couchbase.com>
2014-05-07 21:49:19 -0700

    Revert "Issue #296 Add logging to debug issue"
    This reverts commit 2a93a52f4dce45b4447c28a4180961f406a76eee.

--- src/main/java/com/couchbase/lite/replicator/Replication.java
+++ src/main/java/com/couchbase/lite/replicator/Replication.java
@@ -894,12 +894,6 @@ public abstract class Replication implements NetworkReachabilityListener {
      */
     @InterfaceAudience.Private
     public synchronized void asyncTaskStarted() {
-        try {
-            Log.v(Log.TAG_SYNC, "%s: asyncTaskStarted %d -> %d", this, this.asyncTaskCount, this.asyncTaskCount + 1);
-            dumpPartialStack();
-        } catch (Exception e) {
-            Log.e(Log.TAG_SYNC, "Exception printing stacktrace");
-        }
         if (asyncTaskCount++ == 0) {
             updateActive();
         }
@@ -910,30 +904,10 @@ public abstract class Replication implements NetworkReachabilityListener {
      */
     @InterfaceAudience.Private
     public synchronized void asyncTaskFinished(int numTasks) {
-        try {
-            Log.v(Log.TAG_SYNC, "%s: asyncTaskFinished %d -> %d", this, this.asyncTaskCount, this.asyncTaskCount - numTasks);
-            dumpPartialStack();
-        } catch (Exception e) {
-            Log.e(Log.TAG_SYNC, "Exception printing stacktrace");
-        }
         this.asyncTaskCount -= numTasks;
         assert(asyncTaskCount >= 0);
         if (asyncTaskCount == 0) {
             updateActive();
-        }
-    }
-
-    private void dumpPartialStack() {
-        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
-        if (stackTraceElements.length >= 6) {
-            int counter = 0;
-            for (int i=0; i<6; i++) {
-                StackTraceElement stackTraceElement = stackTraceElements[i];
-                if (stackTraceElement.getClassName().startsWith("com.couchbase") &&
-                        !stackTraceElement.getMethodName().contains("dumpPartialStack")) {
-                    Log.v(Log.TAG_SYNC, "[%d] %s:%s:%d", counter++, stackTraceElement.getClassName(), stackTraceElement.getMethodName(), stackTraceElement.getLineNumber());
-                }
-            }
         }
     }
 
