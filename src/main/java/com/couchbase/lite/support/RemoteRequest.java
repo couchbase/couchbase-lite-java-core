523dbe121ec521e9791d2c58c3a9dc270754f948
Traun Leyden <traun@couchbase.com>
2014-05-08 09:52:48 -0700

    Issue #296 Attempt to diagnose root cause by rethrowing unexpected exceptions
    https://github.com/couchbase/couchbase-lite-android/issues/296

--- src/main/java/com/couchbase/lite/support/RemoteRequest.java
+++ src/main/java/com/couchbase/lite/support/RemoteRequest.java
@@ -86,21 +86,28 @@ public class RemoteRequest implements Runnable {
     @Override
     public void run() {
 
-        HttpClient httpClient = clientFactory.getHttpClient();
+        try {
+            
+            HttpClient httpClient = clientFactory.getHttpClient();
 
-        ClientConnectionManager manager = httpClient.getConnectionManager();
+            ClientConnectionManager manager = httpClient.getConnectionManager();
 
-        HttpUriRequest request = createConcreteRequest();
+            HttpUriRequest request = createConcreteRequest();
 
-        preemptivelySetAuthCredentials(httpClient);
+            preemptivelySetAuthCredentials(httpClient);
 
-        request.addHeader("Accept", "multipart/related, application/json");
+            request.addHeader("Accept", "multipart/related, application/json");
 
-        addRequestHeaders(request);
+            addRequestHeaders(request);
 
-        setBody(request);
+            setBody(request);
 
-        executeRequest(httpClient, request);
+            executeRequest(httpClient, request);
+
+        } catch (Exception e) {
+            Log.e(Log.TAG_REMOTE_REQUEST, "caught and rethrowing unexpected exception: ", e);
+            throw new RuntimeException(e);
+        }
 
     }
 
