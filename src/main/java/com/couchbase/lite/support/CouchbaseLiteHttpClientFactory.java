42175cabe584a22bb0b440bcad2dbbc58fdf7956
Traun Leyden <tleyden@couchbase.com>
2014-12-15 16:02:33 -0800

    add allowSelfSignedSSLCertificates to make testing easier
    https://github.com/couchbase/couchbase-lite-java-core/issues/300

--- src/main/java/com/couchbase/lite/support/CouchbaseLiteHttpClientFactory.java
+++ src/main/java/com/couchbase/lite/support/CouchbaseLiteHttpClientFactory.java
@@ -16,8 +16,22 @@ import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class CouchbaseLiteHttpClientFactory implements HttpClientFactory {
 
@@ -125,5 +139,68 @@ public class CouchbaseLiteHttpClientFactory implements HttpClientFactory {
     public CookieStore getCookieStore() {
         return cookieStore;
     }
+
+    static class SelfSignedSSLSocketFactory extends SSLSocketFactory {
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+
+        public SelfSignedSSLSocketFactory(KeyStore truststore) throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException, UnrecoverableKeyException {
+            super(truststore);
+
+            TrustManager tm = new X509TrustManager() {
+                public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                }
+
+                public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                }
+
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+            };
+
+            sslContext.init(null, new TrustManager[] { tm }, null);
+        }
+
+        @Override
+        public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException, UnknownHostException {
+            return sslContext.getSocketFactory().createSocket(socket, host, port, autoClose);
+        }
+
+        @Override
+        public Socket createSocket() throws IOException {
+            return sslContext.getSocketFactory().createSocket();
+        }
+
+    }
+
+    /**
+     * This is a convenience method to allow couchbase lite to connect to servers
+     * that use self-signed SSL certs.
+     *
+     * *DO NOT USE THIS IN PRODUCTION*
+     *
+     * For more information, see:
+     *
+     * https://github.com/couchbase/couchbase-lite-java-core/pull/9
+     * http://stackoverflow.com/questions/2642777/trusting-all-certificates-using-httpclient-over-https
+     */
+    @InterfaceAudience.Public
+    public void allowSelfSignedSSLCertificates() {
+
+        try {
+
+            KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            trustStore.load(null, null);
+            SSLSocketFactory sf = new SelfSignedSSLSocketFactory(trustStore);
+            this.setSSLSocketFactory(sf);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
 
 }
