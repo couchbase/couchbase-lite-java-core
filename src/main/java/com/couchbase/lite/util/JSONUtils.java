d8d6c3ba33af568751036c7c2fa982d357ee8614
hideki <hideki@couchbase.com>
2015-07-24 15:19:24 -0700

    Fixed https://github.com/couchbase/couchbase-lite-android/issues/629  Out-of-Memory caused by push replication

---
+++ src/main/java/com/couchbase/lite/util/JSONUtils.java
@@ -0,0 +1,11 @@
+package com.couchbase.lite.util;
+
+/**
+ * Created by hideki on 7/23/15.
+ */
+public class JSONUtils {
+    public static long estimateMemorySize(Object obj){
+        // TODO: Needs to implement
+        return 0;
+    }
+}
