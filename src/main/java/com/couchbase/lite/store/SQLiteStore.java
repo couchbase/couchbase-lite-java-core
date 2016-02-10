a49e3aad2154eb2fe1e69dba6c3d546fa7388a2c
hideki <hideki@couchbase.com>
2016-02-09 22:36:29 -0800

    Fixed #1020 - delete unused import

--- src/main/java/com/couchbase/lite/store/SQLiteStore.java
+++ src/main/java/com/couchbase/lite/store/SQLiteStore.java
@@ -49,7 +49,6 @@ import com.couchbase.lite.util.TextUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
