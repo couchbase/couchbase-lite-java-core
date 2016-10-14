a85d3b90d5d7bb88e3681a329a834203d9d40b8f
hideki <hideki@couchbase.com>
2016-10-13 20:56:14 -0700

    Fixed #1234 - Major inefficiencies replicating docs with many revisions
    - Ported implementation from iOS - https://github.com/couchbase/couchbase-lite-ios/issues/1242

--- src/main/java/com/couchbase/lite/store/Store.java
+++ src/main/java/com/couchbase/lite/store/Store.java
@@ -183,12 +183,16 @@ public interface Store {
 
     /**
      * Returns IDs of local revisions of the same document, that have a lower generation number.
-     * Does not return revisions whose bodies have been compacted away, or deletion markers.
-     * If 'onlyAttachments' is true, only revisions with attachments will be returned.
+     * If possible, returns only leaf revisions; if none match, returns non-leaves.
+     * @param rev  The revision to look for ancestors of. Only its docID and revID are used.
+     * @param limit  The maximum number of results to return, or if 0, unlimited.
+     * @param outHaveBodies  On return, if not NULL, then *outHaveBodies will be YES if all the
+     * revisions returned have their JSON bodies available, otherwise NO.
+     * @return  An array of revIDs of existing revisions that could be ancestors of `rev`.
      */
     List<String> getPossibleAncestorRevisionIDs(RevisionInternal rev,
                                                 int limit,
-                                                AtomicBoolean onlyAttachments);
+                                                AtomicBoolean outHaveBodies);
 
     /**
      * Returns the most recent member of revIDs that appears in rev's ancestry.
